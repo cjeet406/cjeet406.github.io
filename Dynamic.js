e8a6faa8e2ec5ff9353a2ef258ef27527b1dfb41
cjeet406 <111974745+cjeet406@users.noreply.github.com>
2024-01-05 02:54:12 +0530

    Add files via upload

---
+++ Dynamic.js
@@ -0,0 +1,11 @@
+WebFont.load({
+    google: {
+      families: ["Droid Serif:400,400italic,700,700italic"]
+    }
+  });
+
+  ! function(o, c) {
+    var n = c.documentElement,
+      t = " w-mod-";
+    n.className += t + "js", ("ontouchstart" in o || o.DocumentTouch && c instanceof DocumentTouch) && (n.className += t + "touch")
+  }(window, document);
