790a8a1f39cc942ea8a003030940d6e5b2dd7033
cjeet406 <111974745+cjeet406@users.noreply.github.com>
2024-01-06 17:20:54 +0530

    Update Dynamic.js

--- Dynamic.js
+++ Dynamic.js
@@ -9,3 +9,14 @@ WebFont.load({
       t = " w-mod-";
     n.className += t + "js", ("ontouchstart" in o || o.DocumentTouch && c instanceof DocumentTouch) && (n.className += t + "touch")
   }(window, document);
+
+
+  function ChangeTxtOfHeading(textYouWantToReplace = null){
+    if(textYouWantToReplace == null){
+      $('#headingtxt').attr('style','display:none');
+    }
+    else {
+      $('#headingtxt').html('');
+      $('#headingtxt').html(textYouWantToReplace);
+    }
+  }
