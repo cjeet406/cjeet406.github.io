71202a51c2aceae06d0b9848a3e37d6759904aaa
cjeet406 <111974745+cjeet406@users.noreply.github.com>
2024-01-08 23:58:52 +0530

    Update Dynamic.js

--- Dynamic.js
+++ Dynamic.js
@@ -10,6 +10,10 @@ WebFont.load({
     n.className += t + "js", ("ontouchstart" in o || o.DocumentTouch && c instanceof DocumentTouch) && (n.className += t + "touch")
   }(window, document);
 
+window.onload = function(){
+  ChangeTxtOfHeading("Hello! Hardik This Side!");
+}
+
 
   function ChangeTxtOfHeading(textYouWantToReplace = null){
     if(textYouWantToReplace == null){
